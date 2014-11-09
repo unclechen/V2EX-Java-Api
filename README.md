c5fd0973a3cd4159181b4119f8deec91dd38e712
K0ST <kost@foxmail.com>
2014-11-10 00:10:51 +0800

    modify readme

--- README.md
+++ README.md
@@ -2,9 +2,10 @@
 ---
 Please use these fucking codes
 
-```java
-	public static void main(String[] args) {
+```
+	public static void main(String[] args) {  
 		V2exManager.getSiteInfo(new OnResponse<SiteInfo>() {
+		
 			public void onRespondResult(SiteInfo result) {
 				//DO whatever you want here
 			}
--- src/pro/kost/v2ex/V2ex.java
+++ src/pro/kost/v2ex/V2ex.java
@@ -1,6 +1,15 @@
 package pro.kost.v2ex;
 
+import pro.kost.v2ex.beans.SiteInfo;
+import pro.kost.v2ex.network.AbsRequest.OnResponse;
+
 
 public class V2ex {
-	
+	public static void main(String[] args) {
+		V2exManager.getSiteInfo(new OnResponse<SiteInfo>() {
+			public void onRespondResult(SiteInfo result) {
+				//DO whatever you want here
+			}
+		});
+	}
 }
