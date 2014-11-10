1fcd838d8f597bf86b391ae95962aade36b90785
K0ST <kost@foxmail.com>
2014-11-10 22:54:29 +0800

    modify readme

--- src/pro/kost/v2ex/V2ex.java
+++ src/pro/kost/v2ex/V2ex.java
@@ -1,14 +1,18 @@
 package pro.kost.v2ex;
 
+import pro.kost.v2ex.beans.NodeList;
 import pro.kost.v2ex.beans.SiteInfo;
+import pro.kost.v2ex.beans.SiteStats;
 import pro.kost.v2ex.network.AbsRequest.OnResponse;
 
 
 public class V2ex {
 	public static void main(String[] args) {
-		V2exManager.getSiteInfo(new OnResponse<SiteInfo>() {
-			public void onRespondResult(SiteInfo result) {
-				//DO whatever you want here
+		V2exManager.getNodesAll(new OnResponse<NodeList>() {
+			
+			public void onRespondResult(NodeList result) {
+				// TODO Auto-generated method stub
+				
 			}
 		});
 	}
